83debed5687df9ee11d6683b3bf84d5f6799b7ce
Aditya <adityam874@gmail.com>
2024-06-08 22:27:28 +0530

    first commit

---
+++ test/fuzz/OpenInvariantsTest.t.sol
@@ -0,0 +1,52 @@
+// Have our invariant aka properties
+
+// What are our invariants?
+
+// 1. The total supply of DSC should be less than the toal value of collateral
+// 2. Getter view functions should never revert <- evergreen invariant
+
+
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {Test, console} from "forge-std/Test.sol";
+import {StdInvariant} from "forge-std/StdInvariant.sol";
+import {DeployDSC} from "../../script/DeployDSC.s.sol";
+import {DSCEngine} from "../../src/DSCEngine.sol";
+import {DecentralizedStableCoin} from "../../src/DecentralizedStableCoin.sol";
+import {HelperConfig} from "../../script/HelperConfig.s.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract OpenInvariantTest is StdInvariant, Test {
+    DeployDSC deployer;
+    DSCEngine dsce;
+    DecentralizedStableCoin dsc;
+    HelperConfig helperConfig;
+    address weth;
+    address wbtc;
+
+    function setUp() external {
+        deployer = new DeployDSC();
+        (dsc, dsce, helperConfig) = deployer.run();
+        (,, weth, wbtc,) = helperConfig.activeNetworkConfig();
+        targetContract(address(dsce));
+    }
+
+    function invariant_protocolMustHaveMoreValueThanTotalSupply() public view {
+        // get the value of all the collateral in the protocol
+        // compare it to all the debt (dsc)
+        uint256 totalSupply = dsc.totalSupply();
+        uint256 totalWethDeposited = IERC20(weth).balanceOf(address(dsce));
+        uint256 totalBtcDeposited = IERC20(wbtc).balanceOf(address(dsce));
+
+        uint256 wethValue = dsce.getUsdValue(weth, totalWethDeposited);
+        uint256 wbtcValue = dsce.getUsdValue(wbtc, totalBtcDeposited);
+
+        console.log("weth value: ", wethValue);
+        console.log("wbtc value: ", wbtcValue);
+        console.log("total supply: ", totalSupply);
+
+        assert(wethValue + wbtcValue >= totalSupply);
+    }
+}
